ee3415fc01f474f517becad30d324720eb45f694
ETamez9173 <elitamez17@gmail.com>
2020-04-10 16:33:48 -0500

    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,101 @@
+﻿
+
+CREATE Table Neighborhood (
+  Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+  [Name] VARCHAR(55) NOT NULL
+)
+
+CREATE Table [Owner] (
+  Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+ [Name] VARCHAR(55) NOT NULL,
+  Address VARCHAR(255)  NOT NULL,
+  NeighborhoodId INTEGER  NOT NULL,
+  Phone VARCHAR(55)  NOT NULL,
+ CONSTRAINT FK_Owner_Neighborhood FOREIGN KEY(NeighborhoodId) REFERENCES Neighborhood(Id)
+)
+
+CREATE Table Walker (
+  Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+ [Name] VARCHAR(55) NOT NULL,
+  NeighborhoodId INTEGER  NOT NULL,
+  CONSTRAINT FK_Walker_Neighborhood FOREIGN KEY(NeighborhoodId) REFERENCES Neighborhood(Id),
+)
+
+
+
+
+CREATE TABLE Dog (
+	Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(55) NOT NULL,
+	OwnerId INTEGER NOT NULL,
+	Breed VARCHAR(55) NOT NULL,
+	Notes VARCHAR(255),
+	CONSTRAINT FK_Dog_Owner FOREIGN KEY (OwnerId) REFERENCES [Owner](Id)
+);
+
+CREATE Table Walks (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Date] DATETIME NOT NULL,
+    Duration INTEGER NOT NULL,
+    WalkerId INTEGER,
+    DogId INTEGER,
+    CONSTRAINT FK_Walks_Walker FOREIGN KEY(WalkerId) REFERENCES Walker(Id),
+    CONSTRAINT FK_Walks_Dog FOREIGN KEY(DogId) REFERENCES Dog(Id)
+)
+
+
+
+--Populate each table with data. You should have 
+--2-3 neighborhoods, 5-10 dogs, 4-8 owners, 2-5 walkers 
+--and each walker should have 1-2 walks recorded.
+
+--2-3 neighborhoods is DONE
+INSERT INTO Neighborhood (Name) VALUES ('Victory Point');
+INSERT INTO Neighborhood (Name) VALUES ('DownTown');
+INSERT INTO Neighborhood (Name) VALUES ('NiceVille');
+
+--4-8 owners is DONE
+  
+INSERT INTO Owner ([Name], Address, NeighborhoodId, Phone) VALUES ('Maura', '123 Main Street', 1, '615-555-1212');
+INSERT INTO Owner ([Name], Address, NeighborhoodId, Phone) VALUES ('Catherine', '123 Main Street', 1, '615-555-7777');
+INSERT INTO Owner ([Name], Address, NeighborhoodId, Phone) VALUES ('Joe Jones', '500 Adams Street', 2,'303-922-5555');
+INSERT INTO Owner ([Name], Address, NeighborhoodId, Phone) VALUES ('Sam Smith', '15 Jackson Street', 3, '212-333-6432');
+
+
+  --2-5 walkers is DONE
+
+INSERT INTO Walker (Name, NeighborhoodId) VALUES ('Sue Puppieas', 1);
+INSERT INTO Walker (Name, NeighborhoodId) VALUES ('Jim Coles',1);
+INSERT INTO Walker (Name, NeighborhoodId) VALUES ('Noah Biggs',2);
+INSERT INTO Walker (Name, NeighborhoodId) VALUES ('Jane Short',3);
+
+
+--5-10 dogs is DONE Had to wait until Owners were added to get the OwnerIds
+
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES  ('Angel', 5, 'Weimereiner' ,'Older dog likes to sleep alot');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES  ('Bonita', 6, 'Pit Bull' , 'Really happy and likes to jump up on you');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES  ('Fido', 7, 'Black Lab' , 'Has a thick fur and gets hot on walks');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES  ('Bongo', 8, 'Weiner Dog', 'Likes to chase things');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES  ('Fluffy', 8, 'Poodle', 'Is a big lap dog');
+
+
+
+ --and each walker should have 1-2 walks recorded. Had to wait until Dogs were added to get the DogIds
+
+
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-17, 35, 1, 61);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-17, 35, 1, 62);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-17, 45, 2, 61);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-17, 45, 2, 62);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-17, 30, 3, 63);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-18, 40, 3, 63);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-18, 40, 4, 64);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-18, 35, 4, 64);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-19, 40, 4, 65);
+INSERT INTO Walks([Date], Duration, WalkerId, DogId) VALUES (2020-03-19, 35, 4, 65);
+
+
+
+
+
+
---
+++ SQLQuery3.sql
@@ -0,0 +1,85 @@
+﻿
+--Write a query to return all owners names and the name of their neighborhood.
+
+SELECT o.Name, n.Name 
+From Owner o
+LEFT JOIN Neighborhood n ON o.NeighborhoodId = n.Id
+
+
+--Write a query to return the name and neighborhood of a single owner (can be any Id).
+SELECT o.Name, n.Name 
+From Owner o
+LEFT JOIN Neighborhood n ON o.NeighborhoodId = n.Id
+Where o.Id = 6
+
+--Write a query to return all walkers ordered by their name.
+--NOTE: SQL offers the ability to order by columns in a table.
+
+SELECT *
+FROM Walker
+ORDER BY Name ASC
+
+--Write a query to return a list of unique dog breeds.
+--NOTE: Take a look at SQL SELECT DISTINCT Statement for some guidance.
+
+SELECT DISTINCT Breed FROM Dog;
+
+
+
+--Write a query to return a list of all dog's names along with their owner's name and what neighborhood they live in
+--NOTE: sometimes you need to join more than two tables in a query.
+
+SELECT d.Name, o.Name, n.Name
+From Dog d
+LEFT JOIN Owner o ON o.id = d.OwnerId
+LEFT JOIN Neighborhood n ON o.NeighborhoodId = n.Id
+
+
+
+--Return a list of owners along with a count of how many dogs they have
+--NOTE: SQL has a group by just like LINQ does.
+
+SELECT o.name, COUNT(d.ownerId) AS DogsOwned
+FROM Dog d
+Left Join Owner o ON d.ownerId = o.id
+GROUP BY o.Name
+
+
+--Return a list of walkers along with the amount of walks they've recorded
+
+SELECT Walker.Name, COUNT(Walks.WalkerId) AS TimesWalked
+FROM Walks 
+Left Join Walker  ON Walks.WalkerId = Walker.id
+GROUP BY Walker.Name
+
+
+--Return a list of all neighborhoods with a count of how many walkers 
+--are in each, but do not show neighborhoods that don't have any walkers.
+
+INSERT INTO Neighborhood ([Name]) VALUES ('NoDogLoversVille');
+
+
+SELECT n.[Name] AS Neighborhood, COUNT(w.NeighborhoodId) AS Walkers
+FROM Walker w
+LEFT JOIN Neighborhood n ON w.NeighborhoodId = n.Id
+GROUP BY n.[Name]
+
+
+--Return a list of dogs that have been walked in the past week
+--NOTE: It may help to know how to format dates
+SELECT * 
+FROM Walks
+--WHERE [Date] BETWEEN '2020-03-12 00:00:00' AND GETDATE();
+--WHERE [Date] BETWEEN '2020-03-12 00:00:00' AND '2020-03-26 00:00:00';
+--WHERE [Date] BETWEEN #6/22/1905# #12:00:00# AND #6/24/1905 12:00:00 AM#;
+
+
+
+--Return a list of dogs that have not been on a walk
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES  ('Buddy', 5, 'Snauzer' ,'Never Goes out');
+
+SELECT d.[Name], d.Breed
+FROM Dog d
+LEFT JOIN Walks w ON d.Id = w.DogId
+WHERE w.Id IS NULL
+GROUP BY d.[Name], d.Breed
